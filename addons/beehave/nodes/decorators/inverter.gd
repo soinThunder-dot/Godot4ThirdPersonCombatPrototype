25dc9a190a3307838b617c895ed94ddcbbbf649b
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-24 23:23:27 +0800

    docs: add Traditional Chinese comments to inverter.gd

--- addons/beehave/nodes/decorators/inverter.gd
+++ addons/beehave/nodes/decorators/inverter.gd
@@ -4,6 +4,7 @@ class_name InverterDecorator extends Decorator
 
 ## An inverter will return `FAILURE` in case it's child returns a `SUCCESS` status
 ## code or `SUCCESS` in case its child returns a `FAILURE` status code.
+## 反轉裝飾器：若子節點回傳 SUCCESS 則轉為 FAILURE，若回傳 FAILURE 則轉為 SUCCESS
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	var c = get_child(0)
@@ -19,19 +20,20 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 		blackboard.set_value("last_condition", c, str(actor.get_instance_id()))
 		blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
 
-	match response:
-		SUCCESS:
+	    # 依照回傳狀態進行反轉處理
+    match response:
+		SUCCESS: # 若子節點成功，則後處理並回傳 FAILURE
 			c.after_run(actor, blackboard)
 			return FAILURE
-		FAILURE:
+		FAILURE: # 若子節點失敗，則後處理並回傳 SUCCESS
 			c.after_run(actor, blackboard)
 			return SUCCESS
-		RUNNING:
+		RUNNING: # 若子節點仍在執行中，則記錄為目前執行中的子節點並回傳 RUNNING
 			running_child = c
 			if c is ActionLeaf:
 				blackboard.set_value("running_action", c, str(actor.get_instance_id()))
 			return RUNNING
-		_:
+		_: # 不應發生的情況，若發生則輸出錯誤
 			push_error("This should be unreachable")
 			return -1
 
