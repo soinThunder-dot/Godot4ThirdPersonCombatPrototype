860b7347ce100b85352c451d6b082f3939ff6745
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-27 12:58:24 +0800

    為 condition.gd 新增繁體中文註解

--- addons/beehave/nodes/leaves/condition.gd
+++ addons/beehave/nodes/leaves/condition.gd
@@ -4,6 +4,8 @@ class_name ConditionLeaf extends Leaf
 
 ## Conditions are leaf nodes that either return SUCCESS or FAILURE depending on
 ## a single simple condition. They should never return `RUNNING`.
+## 條件葉節點：根據一個簡单的條件回傳 SUCCESS 或 FAILURE。
+## 不應回傳 `RUNNING`。
 
 func get_class_name() -> Array[StringName]:
 	var classes := super()
