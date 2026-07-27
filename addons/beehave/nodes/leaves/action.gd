eb2bde45fb2c23801a361ca560a1a4b36e9a8056
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-27 12:10:04 +0800

    為 action.gd 新增繁體中文註解

--- addons/beehave/nodes/leaves/action.gd
+++ addons/beehave/nodes/leaves/action.gd
@@ -6,6 +6,8 @@ class_name ActionLeaf extends Leaf
 ## Their execution can be long running, potentially being called across multiple
 ## frame executions. In this case, the node should return `RUNNING` until the
 ## action is completed.
+## 動作葉節點：定義一個由行為主體（actor）執行的任務。
+## 其執行可能持續多幀，此時應回傳 `RUNNING`，直到動作完成為止。
 
 func get_class_name() -> Array[StringName]:
 	var classes := super()
