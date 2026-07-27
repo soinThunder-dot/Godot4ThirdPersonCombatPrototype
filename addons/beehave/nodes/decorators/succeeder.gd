72c7f16fa1124b26a91f642dad2ebaf35f85b842
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-27 09:54:55 +0800

    docs: add Traditional Chinese comments to succeeder.gd

--- addons/beehave/nodes/decorators/succeeder.gd
+++ addons/beehave/nodes/decorators/succeeder.gd
@@ -3,6 +3,7 @@
 class_name AlwaysSucceedDecorator extends Decorator
 
 ## A succeeder node will always return a `SUCCESS` status code.
+## 永成功裝飾器：無論子節點回傳結果為何，皆一律回傳 SUCCESS
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	var c = get_child(0)
@@ -19,11 +20,13 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 		blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
 
 	if response == RUNNING:
+			# 若子節點仍在執行中，則記錄為目前執行中的子節點並回傳 RUNNING
 		running_child = c
 		if c is ActionLeaf:
 			blackboard.set_value("running_action", c, str(actor.get_instance_id()))
 		return RUNNING
 	else:
+			# 若子節點已完成（非 RUNNING），則進行後處理並回傳 SUCCESS
 		c.after_run(actor, blackboard)
 		return SUCCESS
 
