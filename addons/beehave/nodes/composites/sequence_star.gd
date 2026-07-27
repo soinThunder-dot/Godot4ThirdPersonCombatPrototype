736452d4a70196e32f464138cb358af6c7e389ce
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-27 11:40:54 +0800

    為 sequence_star.gd 新增繁體中文註解

--- addons/beehave/nodes/composites/sequence_star.gd
+++ addons/beehave/nodes/composites/sequence_star.gd
@@ -8,14 +8,17 @@ class_name SequenceStarComposite extends Composite
 ## return `FAILURE` and tick again.
 ## In case a child returns `RUNNING` this node will tick again.
 
+# 記錄目前已成功執行到的子節點索引
 var successful_index: int = 0
 
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	for c in get_children():
+		# 跳過已經成功執行過的子節點
 		if c.get_index() < successful_index:
 			continue
 
+		# 若子節點不是目前執行中的節點，先執行 before_run
 		if c != running_child:
 			c.before_run(actor, blackboard)
 
@@ -23,25 +26,31 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 		if can_send_message(blackboard):
 			BeehaveDebuggerMessages.process_tick(c.get_instance_id(), response)
 
+		# 若子節點是條件節點，則記錄最後的條件與其結果
 		if c is ConditionLeaf:
 			blackboard.set_value("last_condition", c, str(actor.get_instance_id()))
 			blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
 
 		match response:
 			SUCCESS:
+				# 成功則前進到下一個子節點
 				successful_index += 1
 				c.after_run(actor, blackboard)
 			FAILURE:
 				# Interrupt any child that was RUNNING before
 				# but do not reset!
+				# 中斷之前正在 RUNNING 的子節點，但不重置成功索引（下次從原處重試）
 				super.interrupt(actor, blackboard)
 				c.after_run(actor, blackboard)
 				return FAILURE
 			RUNNING:
+				# 記錄為目前執行中的子節點並立即回傳
 				running_child = c
 				if c is ActionLeaf:
 					blackboard.set_value("running_action", c, str(actor.get_instance_id()))
 				return RUNNING
+
+	# 所有子節點都成功完成時，重置並回傳 SUCCESS
 	_reset()
 	return SUCCESS
 
@@ -50,8 +59,9 @@ func interrupt(actor: Node, blackboard: Blackboard) -> void:
 	_reset()
 	super(actor, blackboard)
 
-	
+
 func _reset() -> void:
+	# 重置成功索引，下次從第一個子節點重新開始
 	successful_index = 0
 
 
