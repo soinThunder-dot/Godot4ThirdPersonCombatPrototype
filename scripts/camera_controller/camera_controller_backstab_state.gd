eda0b2a5433da37eaf01d82f16e5348240f84823
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-28 17:38:38 +0800

    Update camera_controller_backstab_state.gd

--- scripts/camera_controller/camera_controller_backstab_state.gd
+++ scripts/camera_controller/camera_controller_backstab_state.gd
@@ -90,98 +90,98 @@ func enter() -> void:
 		360.0
 	)
 
-	func process_camera() -> void:
-	var backstab_victim: BackstabComponent = backstab_system.backstab_victim
-	
-	if backstab_victim:
-		_backstab_behaviour = true
-		_saved_backstab_victim = backstab_victim
-	elif _before_transition_out_timer.is_stopped():
-		_before_transition_out_timer.start()
-	
-	if not _saved_backstab_victim:
-		parent_state.change_state(
-			normal_state
-		)
-		return
-	
-	if _backstab_behaviour:
-		
-		camera.fov = move_toward(
-			camera.fov,
-			65,
-			1
-		)
-		
-		camera_controller.global_position = camera_controller.global_position.lerp(
-			player.global_position + Vector3(0, 0.5, 0), 
-			0.05
-		)
-		
-		camera.look_at(_saved_backstab_victim.global_position)
-		camera.global_rotation_degrees.y += _cam_right_or_left * 20
-		camera.global_rotation_degrees.x -= 10
-		
-		camera_controller.rotation_degrees.x = lerp_angle(
-			camera_controller.rotation_degrees.x,
-			-35.0,
-			0.05
-		)
-		
-		var _looking_direction: Vector3 = camera_controller\
-			.global_position\
-			.direction_to(_saved_backstab_victim.global_position)
-		_looking_direction = -_looking_direction
-		
-		var _target_look: float = atan2(
-			_looking_direction.x,
-			_looking_direction.z
-		
-		)
-		_target_look += _cam_right_or_left * deg_to_rad(90)
-		
-		var desired_rotation_y: float = lerp_angle(
-			camera_controller.rotation.y, 
-			_target_look, 
-			0.1
-		)
-		
-		camera_controller.rotation.y = desired_rotation_y
-		
-	if 182 < diff_in_rotation and diff_in_rotation < 345:
-		_cam_right_or_left = -1
-	else:
-		
-		if _late_transition_timer.is_stopped():
-			_late_transition_timer.start()
-		
-		camera.fov = move_toward(
-			camera.fov,
-			camera_controller.camera_fov,
-			0.5
-		)
-		
-		camera_controller.global_position = camera_controller\
-			.global_position\
-			.lerp(
-				player.global_position + Vector3(
-					0,
-					camera_controller.vertical_offset,
-					0
-				),
-				0.1
-			)
-		
-		camera.rotation = camera.rotation.lerp(Vector3.ZERO, 0.1)
-		
-		camera_controller.rotation.y = lerp_angle(
-			camera_controller.rotation.y,
-			player.rotation.y,
-			0.1
-		)
-		
-		if abs(
-			camera_controller.rotation.y \
-				- player.rotation.y
-		) < 0.05:
-			_saved_backstab_victim = null
+# func process_camera() -> void:
+# 	var backstab_victim: BackstabComponent = backstab_system.backstab_victim
+# 	
+# 	if backstab_victim:
+# 		_backstab_behaviour = true
+# 		_saved_backstab_victim = backstab_victim
+# 	elif _before_transition_out_timer.is_stopped():
+# 		_before_transition_out_timer.start()
+# 	
+# 	if not _saved_backstab_victim:
+# 		parent_state.change_state(
+# 			normal_state
+# 		)
+# 		return
+# 	
+# 	if _backstab_behaviour:
+# 		
+# 		camera.fov = move_toward(
+# 			camera.fov,
+# 			65,
+# 			1
+# 		)
+# 		
+# 		camera_controller.global_position = camera_controller.global_position.lerp(
+# 			player.global_position + Vector3(0, 0.5, 0), 
+# 			0.05
+# 		)
+# 		
+# 		camera.look_at(_saved_backstab_victim.global_position)
+# 		camera.global_rotation_degrees.y += _cam_right_or_left * 20
+# 		camera.global_rotation_degrees.x -= 10
+# 		
+# 		camera_controller.rotation_degrees.x = lerp_angle(
+# 			camera_controller.rotation_degrees.x,
+# 			-35.0,
+# 			0.05
+# 		)
+# 		
+# 		var _looking_direction: Vector3 = camera_controller\
+# 			.global_position\
+# 			.direction_to(_saved_backstab_victim.global_position)
+# 		_looking_direction = -_looking_direction
+# 		
+# 		var _target_look: float = atan2(
+# 			_looking_direction.x,
+# 			_looking_direction.z
+# 			
+# 		)
+# 		_target_look += _cam_right_or_left * deg_to_rad(90)
+# 		
+# 		var desired_rotation_y: float = lerp_angle(
+# 			camera_controller.rotation.y, 
+# 			_target_look, 
+# 			0.1
+# 		)
+# 		
+# 		camera_controller.rotation.y = desired_rotation_y
+# 		
+# 		if 182 < diff_in_rotation and diff_in_rotation < 345:
+# 			_cam_right_or_left = -1
+# 	else:
+# 		
+# 		if _late_transition_timer.is_stopped():
+# 			_late_transition_timer.start()
+# 		
+# 		camera.fov = move_toward(
+# 			camera.fov,
+# 			camera_controller.camera_fov,
+# 			0.5
+# 		)
+# 		
+# 		camera_controller.global_position = camera_controller\
+# 			.global_position\
+# 			.lerp(
+# 				player.global_position + Vector3(
+# 					0,
+# 					camera_controller.vertical_offset,
+# 					0
+# 				),
+# 				0.1
+# 			)
+# 		
+# 		camera.rotation = camera.rotation.lerp(Vector3.ZERO, 0.1)
+# 		
+# 		camera_controller.rotation.y = lerp_angle(
+# 			camera_controller.rotation.y,
+# 			player.rotation.y,
+# 			0.1
+# 		)
+# 		
+# 		if abs(
+# 			camera_controller.rotation.y \
+# 			- player.rotation.y
+# 		) < 0.05:
+# 			_saved_backstab_victim = null
