3d1fcd69dcf4dda41038fbc19e6a16c1d35b29a2
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-28 12:10:12 +0800

    ZH REMARK player_instability_bar.gd

--- scripts/user_interface/player_instability_bar.gd
+++ scripts/user_interface/player_instability_bar.gd
@@ -1,14 +1,17 @@
+# 玩家不穩定値顯示條：顯示玩家目前不穩定値進度，達到滿值時播放特殊動畫及發光效果
 class_name PlayerInstabilityBar
 extends Control
 
-
+# 顏色漸變，依不穩定百分比取樣以改變顯示顏色
 @export var color_gradient: Gradient
 
 var _instability: float
 var _max_instability: float
 
+# 記錄不穩定條圓形初始的 x 軸縮放值，作為滿值基準
 var _default_instability_scale_x: float
 
+# 控制不穩定値歸零後延遲多久才隱藏條的計時器
 var _disappear_pause_timer: Timer
 var _disappear_pause_length: float = 0.5
 
@@ -25,6 +28,7 @@ func _ready():
 	_default_instability_scale_x = _instability_bar.scale.x
 	_max_instability = _player.instability_component.max_instability
 	
+	# 初始化隱藏延遲計時器，時間一到就隱藏條圖
 	_disappear_pause_timer = Timer.new()
 	_disappear_pause_timer.wait_time = _disappear_pause_length
 	_disappear_pause_timer.autostart = false
@@ -34,6 +38,7 @@ func _ready():
 	)
 	add_child(_disappear_pause_timer)
 	
+	# 初始不啟用動畫播放器，並在播放完成後自動關閉
 	_anim_player.active = false
 	_anim_player.animation_finished.connect(
 		func(_anim_name: String): _anim_player.active = false
@@ -43,6 +48,7 @@ func _ready():
 func _process(_delta):
 	_instability = _player.instability_component.instability
 	
+	# 有不穩定値時顯示，歸零且仍顯示中則開始延遲隱藏計時
 	if _instability > 0:
 		visible = true
 	elif _disappear_pause_timer.is_stopped() and visible:
@@ -50,28 +56,34 @@ func _process(_delta):
 	
 	var instability_percentage = _instability / _max_instability
 	
+	# 依百分比線性插值縮放條圖形
 	_instability_bar.scale.x = lerp(
-		0.0, 
+		0.0,
 		_default_instability_scale_x,
 		instability_percentage
 	)
 	
+	# 依百分比從顏色漸變取得對應顏色
 	_instability_bar.self_modulate = color_gradient.sample(
 		instability_percentage
 	)
 	
+	# 達到滿值時顯示發光特效
 	_glare.visible = is_equal_approx(instability_percentage, 1.0)
 
 
+# 播放達到最大不穩定値時的特殊動畫
 func play_max_instability() -> void:
 	_anim_player.active = true
 	_anim_player.play(&"max_instability")
 
 
+# 重置動畫回到初始狀態
 func reset() -> void:
 	_anim_player.active = true
 	_anim_player.play(&"RESET")
 
 
+# 隱藏不穩定値顯示條
 func hide_bar() -> void:
 	visible = false
