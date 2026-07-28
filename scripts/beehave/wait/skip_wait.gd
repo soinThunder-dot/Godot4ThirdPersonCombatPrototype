e508ca87688fe2b5121f6b515700f56e4b826e62
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-28 12:51:53 +0800

    ZH REMARK skip_wait.gd

--- scripts/beehave/wait/skip_wait.gd
+++ scripts/beehave/wait/skip_wait.gd
@@ -1,8 +1,11 @@
+# 跳過等待節點：將指定編號的等待狀態設為假，以跳過該次等待
 class_name SkipWait
 extends ActionLeaf
 
+# 要跳過的等待編號
 @export var wait_id: int
 
 func tick(_actor: Node, blackboard: Blackboard) -> int:
+	# 在黑板上設定對應的等待標記為假，表示不需要再等待
 	blackboard.set_value("wait_" + str(wait_id), false)
 	return SUCCESS
