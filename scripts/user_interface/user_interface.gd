e867f0022cee2b99fe29ec5c68ad514f83861546
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-28 12:10:48 +0800

    ZH REMARK user_interface.gd

--- scripts/user_interface/user_interface.gd
+++ scripts/user_interface/user_interface.gd
@@ -1,6 +1,6 @@
+# 使用者介面根節點：整合管理 HUD、檢查點介面與死亡畫面等子介面元件
 class_name UserInterface
 extends Control
-
 
 @onready var hud: HeadsUpDisplay = $HUD
 @onready var checkpoint_interface: CheckpointInterface = $CheckpointInterface
