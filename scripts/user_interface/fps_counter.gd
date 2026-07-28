ff25cdd9184da6770db788783c86aee6e09b75ff
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-28 11:51:08 +0800

    為 fps_counter.gd 新增繁體中文行內註解

--- scripts/user_interface/fps_counter.gd
+++ scripts/user_interface/fps_counter.gd
@@ -1,5 +1,7 @@
+# FPS 計數器：即時顯示目前遊戲畫面更新率（每秒幀數）
 extends Label
 
 
 func _physics_process(_delta: float) -> void:
+	# 從引擎效能監控器取得目前 FPS 值並顯示在文字上
 	text = "FPS: " + str(Performance.get_monitor(Performance.TIME_FPS))
