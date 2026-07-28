b30b7770833fd95a23a46a60ac2cda708990e6a5
5thunderHEARTpirece <sointangho@gmail.com>
2026-07-28 12:11:22 +0800

    ZH REMARK wellbeing_widget.gd

--- scripts/user_interface/wellbeing_widget.gd
+++ scripts/user_interface/wellbeing_widget.gd
@@ -1,6 +1,6 @@
+# NPC 健康狀態小部件：整合顯示 NPC 的血條與不穩定値條
 class_name WellbeingWidget
 extends Node2D
-
 
 @onready var health_bar: NPCHealthBar = $NPCHealthBar
 @onready var instability_bar: NPCInstabilityBar = $NPCInstabilityBar
